86aedbb3552e467a929f0dcfddec4da04645491f
Woraya : Mint <wb743@nyu.edu>
2017-04-28 21:05:05 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://<dbuser>:<dbpassword>@ds113650.mlab.com:13650/netmedia' 
+    // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
